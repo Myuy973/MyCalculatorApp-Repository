43d36f0c679a2df084f3d5530fc044d960129694
matumoto-yu <songbenyuzai@gmail.com>
2021-02-06 12:40:36 +0900

    計算機能・Clear/ClearEntry機能を実装

--- app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -2,10 +2,108 @@ package com.example.mycalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import com.example.mycalculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var list: MutableList<String> = mutableListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            zeroButton.setOnClickListener { setAndOutput("0")}
+            oneButton.setOnClickListener { setAndOutput("1")}
+            twoButton.setOnClickListener { setAndOutput("2")}
+            threeButton.setOnClickListener { setAndOutput("3")}
+            fourButton.setOnClickListener { setAndOutput("4")}
+            fiveButton.setOnClickListener { setAndOutput("5")}
+            sixButton.setOnClickListener { setAndOutput("6")}
+            sevenButton.setOnClickListener { setAndOutput("7")}
+            eightButton.setOnClickListener { setAndOutput("8")}
+            nineButton.setOnClickListener { setAndOutput("9")}
+        }
+
+        binding.apply {
+            plusButton.setOnClickListener { setAndClear("+")}
+            minusButton.setOnClickListener { setAndClear("-")}
+            divisionButton.setOnClickListener { setAndClear("*")}
+            multiplicationButton.setOnClickListener { setAndClear("/")}
+        }
+
+        binding.apply {
+            clearButton.setOnClickListener { clearTextView()}
+            clearEntryButton.setOnClickListener { clearTextAndList()}
+            backButton.setOnClickListener { }
+        }
+
+        binding.equalButton.setOnClickListener { output(list) }
+
     }
+
+    private fun setAndOutput(numText: String) {
+        var beforetext = binding.numTextView.text.toString()
+        if (beforetext == "0") beforetext = ""
+        binding.numTextView.text = beforetext + numText
+    }
+
+    private fun setAndClear(mark: String) {
+        var inputNumText = binding.numTextView.text.toString()
+        list.add(inputNumText)
+        list.add(mark)
+        binding.numTextView.text = ""
+    }
+
+    private fun clearTextView() {
+        binding.numTextView.text = "0"
+        Log.d("value", "${list}")
+    }
+
+    private fun clearTextAndList() {
+        binding.numTextView.text = "0"
+        list = mutableListOf()
+        Log.d("value", "${list}")
+    }
+
+    private fun output(list: MutableList<String>) {
+        var num = 2
+        var markType = 1
+        var sumNum = list[0].toInt()
+
+        var lastNumText = binding.numTextView.text.toString()
+        list.add(lastNumText)
+
+        while (ListSizeCheck(list, num)) {
+            when(list[markType]) {
+                "+" ->  sumNum += list[num].toInt()
+                "-" ->  sumNum -= list[num].toInt()
+                "*" ->  sumNum *= list[num].toInt()
+                "/" ->  sumNum /= list[num].toInt()
+            }
+
+            markType += 2
+            num += 2
+        }
+
+        binding.numTextView.text = sumNum.toString()
+        Log.d("value", "${list}")
+    }
+
+    private fun ListSizeCheck(list: MutableList<String>, num: Int): Boolean {
+        try {
+            list[num]
+        } catch (e: ArrayIndexOutOfBoundsException) {
+            return false
+        } catch (e: IndexOutOfBoundsException) {
+            return false
+        }
+        return true
+    }
+
+
 }
